8a41937aab8c4cb4d285e7f58bc919718ae8a685
song <shinobu2624@unist.ac.kr>
2021-09-15 01:39:05 +0900

    clearfy code

--- ps.py
+++ ps.py
@@ -15,30 +15,3 @@ def solution(participant, completion):
             answer = candidate
 
     return answer
-
-def test_sol() :
-    test_fa
-
-    testnum = 1
-    # test 1
-    if solution(["leo", "kiki", "eden"], ["eden", "kiki"]) != "leo" :
-        print("test fail ... at " + str(testnum))
-        isAllTestPass = False
-    testnum += 1
-
-    # test 2
-    if solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]) != "vinko" :
-        print("test fail ... at " + str(testnum))
-        isAllTestPass = False
-    testnum += 1
-
-    # test 3
-    if solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]) != "mislav" :
-        print("test fail ... at " + str(testnum))
-        isAllTestPass = False
-    testnum += 1
-    if isAllTestPass :
-        print("All test passed! congraturation...!!")
-
-
-test_sol()
--- testmaker.py
+++ testmaker.py
@@ -4,13 +4,23 @@ import dotenv
 import colorText
 
 def menu(problemNumber) :
-    print('Select  : t(test by test cases in BOJ), s(submit (not implemented yet)), q(quit) : ' , end='')
-    usr = input()
-    if usr in ['q', 'Q', 'quit'] :
-        exit(0)
-    if usr in ['t', 'T', 'test'] :
-        test()
+    while True :
+        print('Select  : t(Test by test cases in BOJ), c(Change problem), a(Add test case), q(Quit) : ' , end='')
+        usr = input()
+        if usr in ['q', 'Q', 'quit'] :
+            exit(0)
+        if usr in ['t', 'T', 'test'] :
+            # To do : evaluates user output, and compare it with correct output.
+            pass
+        if usr in ['c', 'C', 'change'] :
+            # To do : change the problem that user solve
+            pass
+        if usr in ['a', 'A', 'add'] :
+            # To do : add test cases!
+            pass
 
+        else :
+            print('Type correct option...')
 
 if __name__ == '__main__' :
     print('Type problem number : ', end='')
@@ -21,4 +31,5 @@ if __name__ == '__main__' :
     res = httpHelper.getResponse(URL)
     sampleInputs, sampleOutputs = httpHelper.extractSamples(res.text)
     while True : 
+        menu(problemNumber)
         
